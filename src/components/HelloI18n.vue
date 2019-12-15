492828c96f7b707cd1e9172e860b4b6b1c2828a2
dobrien <dobrien@todopatterson.com>
2019-12-15 11:40:07 +0100

    lesson 2: Introduce Vue i18n through the GUI

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ $t("hello") }}</p>
+    <p>{{ $t("message") }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n"
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
