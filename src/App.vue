86e7063e9f39eeaa0b4bcd47dc77b5e5c0d9dd9e
dobrien <dobrien@todopatterson.com>
2019-12-17 20:40:22 +0100

    lesson-4c: getting the locale from the browser

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ export default {
 
     if (locale) {
       this.$i18n.locale = locale;
+    } else if (navigator.language) {
+      this.$i18n.locale = navigator.language.substring(0, 2);
     }
   }
 };
