ea03370c137943c7b4d7fef980a7490c22e60b25
dobrien <dobrien@todopatterson.com>
2019-12-18 18:42:44 +0100

    lesson-6: Pluralisation

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <p>{{ $t("hello", { name }) }}</p>
+    <!-- <p>{{ $t("hello", { name }) }}</p>
     <p>{{ $t("email", { email }) }}</p>
-    <p>{{ $t("goodbye") }}</p>
+    <p>{{ $t("goodbye") }}</p> -->
+
+    <p>{{ $tc("car", 2) }}</p>
+    <p>{{ $tc("person", 0) }}</p>
+    <p>{{ $tc("person", 1) }}</p>
+    <p>{{ $tc("person", 10, { n: "too many" }) }}</p>
+
+    <p>{{ $tc("apple", 0) }}</p>
+    <p>{{ $tc("apple", 1) }}</p>
+    <p>{{ $tc("apple", 2) }}</p>
+
     <button @click="setLocale('en')" class="button">English</button>
     <button @click="setLocale('es')" class="button">Spanish</button>
     <button @click="setLocale('de')" class="button">German</button>
