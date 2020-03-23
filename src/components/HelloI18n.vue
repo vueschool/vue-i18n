ec2b7345e54f82fec0093c9137b4a47e799ebdb8
dobrien <dobrien@todopatterson.com>
2020-03-23 19:35:53 +0100

    8b - style number formatting

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -16,8 +16,30 @@
     <!-- <p>{{ $d(new Date(), "long") }}</p> -->
     <!-- <p>{{ $d(new Date(), "long", "en-GB") }}</p> -->
 
-    <p>{{ product.name }}: {{ $n(product.price, "currency") }}</p>
-    <p>{{ product.stock }} left in stock</p>
+    <!-- <p>{{ product.name }}: {{ $n(product.price, "currency") }}</p>
+    <p>{{ product.stock }} left in stock</p> -->
+
+    <div>
+      <i18n-n :value="4500.45" format="currency">
+        <template v-slot:currency="slotProps">
+          <span style="color: red; font-size: 25px; font-weight: bold">{{
+            slotProps.currency
+          }}</span>
+        </template>
+        <template v-slot:integer="slotProps">
+          <span style="color:red; font-size:25px">{{ slotProps.integer }}</span>
+        </template>
+        <template v-slot:decimal="slotProps">
+          <span style="color:blue">{{ slotProps.decimal }}</span>
+        </template>
+        <template v-slot:fraction="slotProps">
+          <span style="color:purple">{{ slotProps.fraction }}</span>
+        </template>
+        <template v-slot:group="slotProps">
+          <span style="color:red; font-size:25px">{{ slotProps.group }}</span>
+        </template>
+      </i18n-n>
+    </div>
 
     <button @click="setLocale('en-GB')" class="button">UK</button>
     <button @click="setLocale('en-US')" class="button">USA</button>
